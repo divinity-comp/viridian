d5da757e1aaf80d490afd7aa2775fdcdb129f9bb
divinity-comp <admin@divnitycomputing.com>
2016-07-27 06:07:17 +0100

    aaa

--- js/index.js
+++ js/index.js
@@ -130,7 +130,7 @@ var app = {
 
 			facebookConnectPlugin.login(["public_profile", "user_birthday","user_photos","user_hometown","user_likes","user_work_history","user_location","user_about_me","user_actions.books","user_actions.news","user_likes","user_actions.fitness","user_actions.music","user_actions.video"],
 				fbLoginSuccess,
-				function (error) { console.warn("" + error) }
+				function (error) { console.warn("error " + error) }
 			);
 	}
 };
